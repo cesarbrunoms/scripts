cde575051c3f1064f45291d627c792e9f682f57d
BRUNO CESAR MEDEIROS SIQUEIRA <117839619+cesarbrunoms@users.noreply.github.com>
2022-11-24 08:49:36 -0400

    Update 0.1-enable-execution-script-powershell.ps1

--- win/0.1-enable-execution-script-powershell.ps1
+++ win/0.1-enable-execution-script-powershell.ps1
@@ -1,5 +1,8 @@
 # by bruno.cesar@outlook.it
 # Fonte: https://youtu.be/mmXzYxR7s9c
 
-# Como Habilitar a Execução de Scripts PowerShell no Windows
+# 1 - Para saber se esta desabilitado
+# get-executionpolicy
+
+# 2 - Como Habilitar a Execução de Scripts PowerShell no Windows
 set-ExecutionPolicy unrestricted
