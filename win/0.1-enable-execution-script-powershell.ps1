bbed2be627e012aac7f40b32bce13234a7a72b8c
BRUNO CESAR MEDEIROS SIQUEIRA <117839619+cesarbrunoms@users.noreply.github.com>
2022-11-25 07:26:08 -0400

    Delete 0.1-enable-execution-script-powershell.ps1

--- win/0.1-enable-execution-script-powershell.ps1
+++
@@ -1,8 +0,0 @@
-# by bruno.cesar@outlook.it
-# Fonte: https://youtu.be/mmXzYxR7s9c
-
-# 1 - Para saber se esta desabilitado
-# get-executionpolicy
-
-# 2 - Como Habilitar a Execução de Scripts PowerShell no Windows
-set-ExecutionPolicy unrestricted
