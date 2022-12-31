cb536c880a8eca1645e3b07d59134fbbb842e623
BRUNO CESAR MEDEIROS SIQUEIRA <117839619+cesarbrunoms@users.noreply.github.com>
2022-12-31 15:20:21 -0400

    Delete 00_enable_execution_script_powershell.bat

--- win/00_enable_execution_script_powershell.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-rem *** 1 - Status to know if it is enabled or disabled ***
-PowerShell -Command "get-executionpolicy"
-
-rem *** 2 - Enable execution script powershell ***
-PowerShell -Command "set-ExecutionPolicy unrestricted"
