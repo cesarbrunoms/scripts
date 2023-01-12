21c251f92515924c8c06b22d793b42e3d6900c8e
BRUNO CESAR MEDEIROS SIQUEIRA <117839619+cesarbrunoms@users.noreply.github.com>
2023-01-11 21:52:53 -0400

    Update dti_pma.ps1

--- win/dti_pma.ps1
+++ win/dti_pma.ps1
@@ -8,7 +8,7 @@
 If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
 	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
 	# Enable execution script PowerShell
-	#Start-Process powershell.exe "-command set-ExecutionPolicy unrestricted"
+	Start-Process powershell.exe "-command set-ExecutionPolicy unrestricted"
 	Exit
 }
 
